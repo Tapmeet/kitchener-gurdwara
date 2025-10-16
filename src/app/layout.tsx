68a35475d352be2d751b538337a31530ae1b0616
Tapmeet Singh <tapmeet@gmail.com>
2025-10-16 00:21:31 -0400

    Text Fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
 
           <footer className='border-t border-black/5'>
             <div className='container mx-auto px-4 py-6 text-sm text-gray-500'>
-              © {new Date().getFullYear()} Gurdwara Booking
+              © {new Date().getFullYear()} Kitchener Gurdwara Booking
             </div>
           </footer>
 
